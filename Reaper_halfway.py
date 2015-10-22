b65a9fd8241dd474ebb6aff162875b06ebf609cd
Rjblaauboer <rj.blaauboer@gmail.com>
2015-10-22 18:07:18 +0100

    Added markers.txt and setup filereading

--- Reaper_halfway.py
+++ Reaper_halfway.py
@@ -1,11 +1,26 @@
 __author__ = 'Robert Blaauboer'
+
+import re
+
 #A script that takes a marker file from Reaper
 #then it creates a new marker file where the markers are
 #halfway between the old ones
 
+#data example
+#1 46.19841269841201 "Hammersmith " 0 -1.00000000000000 0
+#2 151.29471655328800 "Barons Court " 0 -1.00000000000000 0
+#3 346.55131519273567 "Earl's Court " 0 -1.00000000000000 0
 
-#import marker file as .txt file
 
+
+def markers_halfway(in_data):
+    markers_tmp = " ".join(in_data)
+    print(markers_tmp)
+
+
+
+
+    return markers_tmp
 
 
 #read through entire file and create list with ID, Time, Description
@@ -29,4 +44,18 @@ __author__ = 'Robert Blaauboer'
 
 
 
-#Output list as new file 
+#Output list as new file
+
+#import marker file as .txt file
+f = open("markers.txt")
+for line in f:
+    in_data = line.split()
+    print("###############ORIGINAL DATA##################")
+    print(in_data)
+    print("##############################################")
+    print()
+#change time of markers
+    out_data = markers_halfway(in_data)
+    print("################PROCESSED#####################")
+    print(out_data)
+    print("##############################################")
