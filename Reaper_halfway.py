b3b9b39e3ec222c83c9574fd3dcb16fa267acbc9
Rjblaauboer <rj.blaauboer@gmail.com>
2015-10-22 19:03:21 +0100

    Get time value through regular expression and substitute with halfway value. Need to correct write to file since new lines are not on a new line

--- Reaper_halfway.py
+++ Reaper_halfway.py
@@ -5,6 +5,7 @@ import re
 #A script that takes a marker file from Reaper
 #then it creates a new marker file where the markers are
 #halfway between the old ones
+#list needs to be ordered acccording to time
 
 #data example
 #1 46.19841269841201 "Hammersmith " 0 -1.00000000000000 0
@@ -13,22 +14,13 @@ import re
 
 
 
-def markers_halfway(in_data):
+def markers_halfway(in_data, time_previous):   # takes one line of text and the marker time of the previous line
+                                # if time is NULL it is assumed that this is the first marker
     markers_tmp = " ".join(in_data)
     print(markers_tmp)
 
 
-
-
-    return markers_tmp
-
-
-#read through entire file and create list with ID, Time, Description
-
-
-
-
-#Replace marker times with marker times at halfway point
+    #Replace marker times with marker times at halfway point
 #example:   1 200 "Cannon Street"   #first marker stays at same place
 #           2 300 "Hammersmith"
 #           3 500 "Embankment"
@@ -42,12 +34,35 @@ def markers_halfway(in_data):
 #           4 650 "Waterloo"
 #           5 800 "Last station"
 
+    re_1 = re.compile("((?<=[\s])\d*\.\d*(?=[\s]))", re.I)
+    time_current = re.search(re_1, markers_tmp)
+    if time_current and time_previous == 'NULL':
+        print("first marker")
+        time_new = time_current.group(0)
+    elif time_current:
+        print("previous time")
+        time_new = (float(time_previous) + float(time_current.group(0))) / 2
+        print("time current: " + time_current.group(0))
+    else:
+        return (markers_tmp, time_previous)
+
+    markers_tmp = re.sub(re_1, str(time_new), markers_tmp)
+
+    return (markers_tmp, time_current.group(0))
+
+
+
+
+
+
 
 
 #Output list as new file
 
 #import marker file as .txt file
 f = open("markers.txt")
+n = open("markers_halfway.txt", 'w')
+time_previous = "NULL"
 for line in f:
     in_data = line.split()
     print("###############ORIGINAL DATA##################")
@@ -55,7 +70,8 @@ for line in f:
     print("##############################################")
     print()
 #change time of markers
-    out_data = markers_halfway(in_data)
+    out_data, time_previous = markers_halfway(in_data, time_previous)
+    n.write(out_data)
     print("################PROCESSED#####################")
-    print(out_data)
+    print(out_data + '\n')
     print("##############################################")
